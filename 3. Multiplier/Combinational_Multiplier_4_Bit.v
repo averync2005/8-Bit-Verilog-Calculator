3e69c248e391f3571310f54108ea7b60a1892c84
Avery Nudell-Cook <averync2005@users.noreply.github.com>
2025-12-01 20:48:04 -0500

    Implemented code

--- 3. Multiplier/Combinational_Multiplier_4_Bit.v
+++ 3. Multiplier/Combinational_Multiplier_4_Bit.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 module Combinational_Multiplier_4_Bit (
-    input [3:0] a,
-    input [3:0] b,
-    output [7:0] p
+    input [3:0] A,
+    input [3:0] B,
+    output [7:0] P
 );
 
     // ------- PARTIAL PRODUCTS -------
@@ -14,14 +14,14 @@ module Combinational_Multiplier_4_Bit (
     // ------- SUM WIRES -------
     wire [7:0] s1, s2, s3;
 
-    assign m0 = a & {4{b[0]}};
-    assign m1 = a & {4{b[1]}};
-    assign m2 = a & {4{b[2]}};
-    assign m3 = a & {4{b[3]}};
+    assign m0 = A & {4{B[0]}};
+    assign m1 = A & {4{B[1]}};
+    assign m2 = A & {4{B[2]}};
+    assign m3 = A & {4{B[3]}};
 
     assign s1 = {4'b0000, m0} + {3'b000, m1, 1'b0};
     assign s2 = s1 + {2'b00, m2, 2'b00};
     assign s3 = s2 + {1'b0, m3, 3'b000};
 
-    assign p = s3;
+    assign P = s3;
 endmodule
